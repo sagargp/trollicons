9128a1b3ec6bbd74ba4d63bff261985e6ab302f5
Sagar Pandya <sagargp@gmail.com>
2011-05-16 00:07:01 -0700

    added gmail chat support

--- browsers/chrome/trollicons.js
+++ browsers/chrome/trollicons.js
@@ -1,16 +1,33 @@
 (function() {
 	jQuery.noConflict();
 	var changing = false;
-	jQuery('body').delegate('div#fbDockChat', 'DOMNodeInserted', function(event) {
+	
+	if (/facebook/.test(window.location.href)) {
+		var chatNode = 'div#fbDockChat';
+		var messageNode = '.fbChatMessage';
+		var imgNode = (function(key, img) {
+				return "<span style=\"display:none\">" + key + "</span><img src=\"" + chrome.extension.getURL("img/" + img) + "\"/ style=\"background-position: 0px 0px\" alt=\":)\">";
+			});
+	} else if (/google/.test(window.location.href)) {
+		var chatNode = 'div.nH.Hd';
+		var messageNode = '.tReiP';
+		var imgNode = (function(key, img) {
+				return "<img src=\"" + chrome.extension.getURL("img/" + img) + "\" style=\"background-image: url(" + chrome.extension.getURL("img/" + img) + ");\"/ alt=\":)\">";
+			});
+	} else {
+		console.debug('not facebook or gmail');
+	}
+	
+	jQuery('body').delegate(chatNode, 'DOMNodeInserted', function(event) {
 		if (changing == false)
 			changing = true;
 		else
 			return;
 
 		var re = new RegExp("\[[a-z]+\]", "g");
-		var messages = jQuery(".fbChatMessage");
+		var messages = jQuery(messageNode);
 		jQuery.each(messages, function() {
-			var oldText = jQuery(this).text();
+			var oldText = jQuery(this).html();
 			var newText = oldText;
 			var matches = oldText.match(re);
 			var message = this;
@@ -19,10 +36,8 @@
 					var key = this.substring(1, this.length-1);
 					var img = rages[key];
 					
-					newText = newText.replace("[" + key + "]", "<span style=\"display:none\">" + key + "</span><img src=\"" + chrome.extension.getURL("img/" + img) + "\"/ style=\"background-position: 0px 0px\" alt=\":)\">");
-					console.debug(key + " --> " + img);
+					newText = newText.replace("[" + key + "]", imgNode(key, img));
 				});
-				console.debug("new: " + newText);
 				jQuery(message).html(newText);
 			}
 		});

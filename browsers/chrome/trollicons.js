4e167fc64da054d16dfae2b534efab7022ab7df3
Sagar Pandya <sagargp@gmail.com>
2011-06-06 17:56:50 -0700

    working on rage faces all over FB

--- browsers/chrome/trollicons.js
+++ browsers/chrome/trollicons.js
@@ -3,8 +3,10 @@
 	var changing = false;
 	
 	if (/facebook/.test(window.location.href)) {
-		var chatNode = 'div#fbDockChat';
-		var messageNode = '.fbChatMessage';
+		var nodes = {};
+		nodes['div#fbDockChat'] = '.fbChatMessage';
+		nodes['div#contentArea'] = '.storyContent'; 
+
 		var imgNode = (function(key, img) {
 				return "<span style=\"display:none\">" + key + "</span><img src=\"" + chrome.extension.getURL("img/" + img) + "\"/ style=\"background-position: 0px 0px\" alt=\":)\">";
 			});
@@ -18,31 +20,39 @@
 		console.debug('not facebook or gmail');
 	}
 	
-	jQuery('body').delegate(chatNode, 'DOMNodeInserted', function(event) {
-		if (changing == false)
-			changing = true;
-		else
-			return;
 
-		var re = new RegExp("\[[a-z]+\]", "g");
-		var messages = jQuery(messageNode);
-		jQuery.each(messages, function() {
-			var oldText = jQuery(this).html();
-			var newText = oldText;
-			var matches = oldText.match(re);
-			var message = this;
-			if (matches) {
-				jQuery.each(matches, function() {
-					var key = this.substring(1, this.length-1);
-					var img = rages[key];
-					
-					newText = newText.replace("[" + key + "]", imgNode(key, img));
-				});
-				jQuery(message).html(newText);
-			}
-		});
+	var re = new RegExp("\[[a-z]+\]", "g");
+
+	jQuery.each(nodes, function(chatNode, messageNode) {
+		jQuery('body').delegate(chatNode, 'DOMNodeInserted', function(event) {
+			if (changing == false)
+				changing = true;
+			else
+				return;
+
+			var messages = jQuery(messageNode);
+			jQuery.each(messages, function() {
+				var oldText = jQuery(this).html();
+				var newText = oldText;
+				var matches = oldText.match(re);
+				var message = this;
+				if (matches) {
+					jQuery.each(matches, function() {
+						var key = this.substring(1, this.length-1);
+						var img = rages[key];
+						
+						if (!img)
+							return true; // this is the same as 'continue' in a $.each()
+
+						newText = newText.replace("[" + key + "]", imgNode(key, img));
+						console.debug('replaced -->' + this + '<-- with ' + newText);
+					});
+					jQuery(message).html(newText);
+				}
+			});
 		
-		changing = false;
+			changing = false;
+		});
 	});
 })();
 

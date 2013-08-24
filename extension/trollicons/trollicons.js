dfb93f472a0a26d2e95b5c6d1c7b56bf022b2229
Sagar Pandya <sagargp@gmail.com>
2013-08-24 00:51:33 -0600

    FB chat plugin works again.
    - Updated for new Chrome manifest version.
    - Untested. Please report bugs here.

--- extension/trollicons/trollicons.js
+++ extension/trollicons/trollicons.js
@@ -1,45 +1,41 @@
-(function() {
+(function()
+{
 	jQuery.noConflict();
 	var changing = false;
+	var nodes = {};
 
 	/* depending on where we are (fb or gmail) define a "replacement node" that we can just use later */
-	if (/facebook/.test(window.location.href)) {
-		var nodes = {};
-		nodes['div#fbDockChat'] = '.fbChatMessage'; // the docked chat window messages
-		//nodes['div#contentArea'] = '.storyContent'; // items in the feeds (this is buggy)
-		nodes['div#MessagingMessages'] = '.MessagingMessage'; // chat messages in the inbox
+	if (/facebook/.test(window.location.href))
+  {
+		nodes['div.conversation'] = '.fbChatConvItem .messages span'; // chat messages in the inbox
+		nodes['li.webMessengerMessageGroup'] = 'span'; // the docked chat window messages
 
-		var imgNode = (function(key, img) {
-				return "<span style=\"display:none\">" + key + "</span><img src=\"" + chrome.extension.getURL(img) + "\"/ style=\"background-position: 0px 0px\" alt=\":)\">";
-			});
+		var imgNode = (function(key, img)
+      {
+		  	return "<span style=\"display:none\">" + key + "</span><img src=\"" + chrome.extension.getURL(img) + "\"/ style=\"background-position: 0px 0px\" alt=\":)\">";
+		  });
 	}
-	/*
-	* else if (/google/.test(window.location.href)) {
-	*   console.debug('gmail plugin is experimental');
-	* 	var chatNode = 'div.nH.Hd';
-	* 	var messageNode = '.tReiP';
-	* 	var imgNode = (function(key, img) {
-	* 			return "<img src=\"" + chrome.extension.getURL(img) + "\" style=\"background-image: url(" + chrome.extension.getURL(img) + ");\"/ alt=\":)\">";
-	* 		});
-	* }
-	*/
-	else {
+	else
+  {
 		console.debug('not facebook');
 	}
 
+  /****
+   * now do the actual replacement of said node (defined above) 
+   ****/
 
-  /* now do the actual replacement of said node (defined above) */
-
-  // this is just going to match any simple strings wrapped in square brackets (ie. [balls] not [hello world])
+  // this is just going to match any lower case single word in square brackets (ie. [foo] not [hello world])
 	var re = new RegExp("\[[a-z]+\]", "g");
 
   // go through the nodes we care about
-	jQuery.each(nodes, function(chatNode, messageNode) {
+	jQuery.each(nodes, function(chatNode, messageNode)
+  {
 	  console.debug("### chatNode = " + chatNode);
 	  console.debug("### messageNode = " + messageNode);
 
-		jQuery('body').delegate(chatNode, 'DOMNodeInserted', function(event) {
-
+    // When the DOM inserts a new node (message) under the chatNode node, do the icon filtering
+		jQuery('body').delegate(chatNode, 'DOMNodeInserted', function(event)
+    {
       // we don't want to respond to the "change" event casued by our own replacement code
 			if (changing == false)
 				changing = true;
@@ -47,14 +43,19 @@
 				return;
 
       // loop through all the messages in the chat window and replace the text with our icons
-			var messages = jQuery(messageNode);
-			jQuery.each(messages, function() {
+			var messages = jQuery(chatNode + " " + messageNode);
+
+			jQuery.each(messages, function()
+      {
 				var oldText = jQuery(this).html();
 				var newText = oldText;
 				var matches = oldText.match(re);
 				var message = this;
-				if (matches) {
-					jQuery.each(matches, function() {
+
+				if (matches)
+        {
+					jQuery.each(matches, function()
+          {
 						var key = this.substring(1, this.length-1);
 						var img = rages[key];
 						
@@ -67,7 +68,6 @@
 					jQuery(message).html(newText);
 				}
 			});
-		
 			changing = false;
 		});
 	});
